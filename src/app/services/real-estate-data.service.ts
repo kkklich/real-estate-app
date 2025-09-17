8ca5e99e9672563d08527c926f6292ed417f092f
krzysztof <krzysztof.klich@idealogic.pl>
2025-09-17 16:36:22 +0200

    add enviroments

--- src/app/services/real-estate-data.service.ts
+++ src/app/services/real-estate-data.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PropertydataAPI } from '../models/property';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class RealEstateDataService {
-    // private readonly apiUrl = 'http://localhost:5016/api/RealEstate'; // replace with your real API URL
-    private readonly apiUrl = 'https://olxapi-jwcz.onrender.com/api/RealEstate'; // replace with your real API URL
+    private readonly apiUrl = environment.apiUrl + '/api/RealEstate';
 
     constructor(private readonly http: HttpClient) { }
 
