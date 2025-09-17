6916f317b27d8f389e50a267957967aeeabc40d5
krzysztof <krzysztof.klich@idealogic.pl>
2025-09-17 11:39:56 +0200

    add url to server

--- src/app/services/real-estate-data.service.ts
+++ src/app/services/real-estate-data.service.ts
@@ -5,7 +5,8 @@ import { PropertydataAPI } from '../models/property';
 
 @Injectable({ providedIn: 'root' })
 export class RealEstateDataService {
-    private readonly apiUrl = 'http://localhost:5016/api/RealEstate'; // replace with your real API URL
+    // private readonly apiUrl = 'http://localhost:5016/api/RealEstate'; // replace with your real API URL
+    private readonly apiUrl = 'https://olxapi-jwcz.onrender.com/api/RealEstate'; // replace with your real API URL
 
     constructor(private readonly http: HttpClient) { }
 
